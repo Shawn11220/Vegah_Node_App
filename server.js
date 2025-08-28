281104e368f7571f0821ee3158f812e593720f4e
Shawn11220 <shawnfrost11220@gmail.com>
2025-08-28 13:48:20 +0530

    env setup 02

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-console.log("MONGO_URI from env:", process.env.MONGO_URI);
+console.log("MONGO_URL from env:", process.env.MONGO_URL);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
